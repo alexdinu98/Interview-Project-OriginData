86bca32585780cfafbd71168b13609bbb373b894
alexdinu98* <alexdinu495@gmail.com>
2021-08-07 19:41:52 +0300

    pushing project to github

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Company;
+use App\Http\Resources\CompanyResource;
+use Illuminate\Http\Request;
+
+
+class CompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $companies = Company::paginate(10);
+        return CompanyResource::collection($companies);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $company = new Company();
+        $company->name = $request->name;
+        $company->address = $request->address;
+        $company->website = $request->website;
+        if($company->save()) {
+            return new CompanyResource($company);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $company = Company::findOrFail($id);
+        return new CompanyResource($company);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $company = Company::findOrFail($id);
+        $company->name = $request->name;
+        $company->address = $request->address;
+        $company->website = $request->website;
+        if($company->save()) {
+            return new CompanyResource($company);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $company = Company::findOrFail($id);
+        if($company->delete()) {
+            return new CompanyResource($company);
+        }
+    }
+}
